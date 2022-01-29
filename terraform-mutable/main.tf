338f5b6a323f027b951e384b30c1c24afd711abd
Gayatri <gayatrikanaparthi@gmail.com>
2022-01-29 22:58:31 +0530

    folder name changed as terraform-mutable

--- terraform-mutable/main.tf
+++ terraform-mutable/main.tf
@@ -9,9 +9,9 @@ module "cart" {
   PORT                = 8080
   IS_PRIVATE_LB       = true
   LB_RULE_PRIORITY    = 105
-#  APP_VERSION         = var.APP_VERSION
-#  NEXUS_USER          = var.NEXUS_USER
-#  NEXUS_PASS          = var.NEXUS_PASS
+  APP_VERSION         = var.APP_VERSION
+  NEXUS_USER          = var.NEXUS_USER
+  NEXUS_PASS          = var.NEXUS_PASS
 }
 
 output "INSTANCE_IDS" {
